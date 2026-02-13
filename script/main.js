5db36a9683d4aa47a2a0c8adb90de66debb9fd15
Shahg-ops <ahsenarshad43@gmail.com>
2026-02-13 19:54:00 +0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -222,7 +222,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "Red",
+        color: "red",
         ease: Expo.easeOut,
       },
       0.1,
